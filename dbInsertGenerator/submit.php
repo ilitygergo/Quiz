f97493638310cf1734b4252d773af8ebe9edd3be
Igor <igor.takacs.98@gmail.com>
2019-03-08 22:04:38 +0100

    Error handling fix - Insert Generator

--- dbInsertGenerator/submit.php
+++ dbInsertGenerator/submit.php
@@ -2,19 +2,29 @@
 if (    isset($_POST['qu']) AND isset($_POST['ra']) AND isset($_POST['wa1']) AND
         isset($_POST['wa2']) AND isset($_POST['wa3'])   ) {
 
-    $question = $_POST['qu'];
-    $right_answer = $_POST['ra'];
-    $wrong_answer1 = $_POST['wa1'];
-    $wrong_answer2 = $_POST['wa2'];
-    $wrong_answer3 = $_POST['wa3'];
+    $question = $_POST['qu'] or null;
+    $right_answer = $_POST['ra'] or null;
+    $wrong_answer1 = $_POST['wa1'] or null;
+    $wrong_answer2 = $_POST['wa2'] or null;
+    $wrong_answer3 = $_POST['wa3'] or null;
     $hardness = $_POST['ha'];
 
-    $output = fopen("insertLines.txt","a");
+    if(     !empty($question) and !empty($right_answer) and !empty($wrong_answer1) and
+            !empty($wrong_answer2) and !empty($wrong_answer3)   ){
 
-    $query_text = "INSERT INTO `Questions` VALUES (NULL, '$hardness', '$right_answer', '$wrong_answer1', '$wrong_answer2', '$wrong_answer3', '$question');\n";
-    fwrite($output,$query_text);
-    fclose($output);
-    header("Location: index.php?err=0");
+        $output = fopen("insertLines.txt","a");
+
+        $query_text = "INSERT INTO `Questions` VALUES (NULL, '$hardness', '$right_answer', '$wrong_answer1', '$wrong_answer2', '$wrong_answer3', '$question');\n";
+        fwrite($output,$query_text);
+        fclose($output);
+
+        header("Location: index.php?err=0");
+
+    } else {
+        header("Location: index.php?err=1");
+    }
+
+
 
 } else {
     header("Location: index.php?err=1");
