d664761f3408553bee131b162d6741d9c75dea0b
Ility Gergő <h659367@stud.u-szeged.hu>
2019-04-18 18:29:31 +0200

    Background color

--- src/private/views/Index.php
+++ src/private/views/Index.php
@@ -4,19 +4,19 @@ require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/Quiz/src/private/etc/menu.p
 
 ?>
 
-<div class="w3-display-container w3-round-large w3-green" style="
+<div class="w3-display-container w3-round-large" style="
     height:400px;
     position: relative;
     top: 150px;
     z-index: 1;
-    background: #FFFFFF;
+    background: #BFC0C0;
     max-width: 390px;
     margin: 0 auto 100px;
     padding: 45px;
     text-align: center;
     box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);">
-    <button type="button" class="btn btn-warning btn-lg" onclick="game()">Quick Game</button>
-    <table class="w3-display-middle w3-panel w3-round-large w3-teal">
+    <button type="button" class="btn btn-info btn-lg" onclick="game()">Quick Game</button>
+    <table class="w3-display-middle w3-panel w3-round-large">
         <tr>
             <td id="themetd">History</td>
             <td id="themetd">
